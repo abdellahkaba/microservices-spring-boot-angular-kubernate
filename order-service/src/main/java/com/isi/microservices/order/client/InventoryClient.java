2f02bbba549db36742203c650147c614603385f2
Abdellah <abdallahkaba98@gmail.com>
2024-08-30 13:03:35 +0000

    circuitbreaker-resilience-and-using-spring-webflux

--- order-service/src/main/java/com/isi/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/isi/microservices/order/client/InventoryClient.java
@@ -1,13 +1,23 @@
 package com.isi.microservices.order.client;
 
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
+
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+    @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode,@RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Impossible d'obtenir l'inventaire pour le skucode {}, raison de l'échec: {}", code, throwable.getMessage());
+        return false;
+    }
 }
