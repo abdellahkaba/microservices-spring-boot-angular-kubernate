68000c8763551218bda678032b0ef7ab47b94d7c
Abdellah <abdallahkaba98@gmail.com>
2024-08-30 10:11:11 +0000

    openFeign-order-and-inventory

---
+++ order-service/src/main/java/com/isi/microservices/order/client/InventoryClient.java
@@ -0,0 +1,13 @@
+package com.isi.microservices.order.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
+public interface InventoryClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    boolean isInStock(@RequestParam String skuCode,@RequestParam Integer quantity);
+}
--- order-service/src/main/java/com/isi/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/isi/microservices/order/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.isi.microservices.order.service;
 
+import com.isi.microservices.order.client.InventoryClient;
 import com.isi.microservices.order.dto.OrderRequest;
 import com.isi.microservices.order.model.Order;
 import com.isi.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -11,17 +13,24 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository repository;
+    private final InventoryClient client ;
     public void placeOrder(OrderRequest request){
-        // Map OrderRequest to Order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(request.price().multiply(BigDecimal.valueOf(request.quantity())));
-        order.setSkuCode(request.skuCode());
-        order.setQuantity(request.quantity());
-        // save in repository
-        repository.save(order) ;
+        var isProductInStock = client.isInStock(request.skuCode(), request.quantity()) ;
+        if (isProductInStock){
+            // Map OrderRequest to Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(request.price().multiply(BigDecimal.valueOf(request.quantity())));
+            order.setSkuCode(request.skuCode());
+            order.setQuantity(request.quantity());
+            // save in repository
+            repository.save(order) ;
+        }else {
+            throw new RuntimeException("Product avec SkuCode " + request.skuCode()+ " non disponible");
+        }
     }
 }
