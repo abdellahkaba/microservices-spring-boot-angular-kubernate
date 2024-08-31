65bbd2970217775bc8275e425271180737307f20
Abdellah <abdallahkaba98@gmail.com>
2024-08-31 15:12:03 +0000

    config-grafana-loki-tempo-prometheus

--- order-service/src/main/java/com/isi/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/isi/microservices/order/config/RestClientConfig.java
@@ -1,6 +1,8 @@
 package com.isi.microservices.order.config;
 
 import com.isi.microservices.order.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
@@ -13,16 +15,19 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.time.Duration;
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
 
     @Value("${inventory.service.url}")
     private String inventoryServiceUrl;
+    private final ObservationRegistry observationRegistry;
 
     @Bean
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
                 .requestFactory(getClientRequestFactory())
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
